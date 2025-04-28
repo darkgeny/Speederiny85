2de975826f789db883e0faf03070e8b42d0a1f5b
darkgeny <117039007+darkgeny@users.noreply.github.com>
2025-04-28 23:14:04 +0200

    Add files via upload
    with this debug, now we have two serial ports that working simultaneously

--- OpticSensor_send_speed_serial_simulation/OpticSensor_send_speed_serial_simulation.ino
+++ OpticSensor_send_speed_serial_simulation/OpticSensor_send_speed_serial_simulation.ino
@@ -21,44 +21,50 @@ void setup(){
   Serial.println("Serial started!");
 
   optic_A_attiny85.begin(38400);
-  //delay(1000);
-  //optic_B_attiny85.begin(38400);
-  //delay(1000);
+  optic_B_attiny85.begin(38400);
   take_value_optic_A.set_local_speed_interval(1000);
-  //take_value_optic_B.set_local_speed_interval(1000);
-  Serial.println("Set interval for take speed every 1000 milliseconds.\n(otherwise, default value is 500).");
+  take_value_optic_B.set_local_speed_interval(1000);
+  Serial.println("\n\n\n\n\n\n\nI have set interval -for take speed- every 1000 milliseconds.\n(otherwise, default value is 500).");
+  Serial.println("Please send command aa to take speed from sensor A and bb to take steps from sensor B.\n");
 }
 void loop(){
   char c = '\0';
   if (Serial.available())
     c = Serial.read();
-  if( c )
-    take_value_optic_A.tvserial->write( c );
-    optic_A_attiny85.write( c );
 
-  take_value_optic_A.take();
+  if( c == 'a' ){
+    if( !optic_A_attiny85.isListening() )
+      optic_A_attiny85.listen();
 
-  if( take_value_optic_A.have_taked_speed() ){
-    Serial.print("speed A taken:");
-    Serial.println( take_value_optic_A.get_speed() );
-  }
-  if( take_value_optic_A.have_taked_steps() ){
-    Serial.print("steps A taken:");
-    Serial.println( take_value_optic_A.get_steps() );
-  }
-/*
-  if( c )
-    optic_B_attiny85.write( c );
+    optic_A_attiny85.write( "#s\n" );
 
-  take_value_optic_B.take();
+    take_value_optic_A.take();
 
-  if( take_value_optic_B.have_taked_speed() ){
-    Serial.print("speed B taken:");
-    Serial.println( take_value_optic_B.get_speed() );
+    if( take_value_optic_A.have_taked_speed() ){
+      Serial.print("speed A taken:");
+      Serial.println( take_value_optic_A.get_speed() );
+    }
+    if( take_value_optic_A.have_taked_steps() ){
+      Serial.print("steps A taken:");
+      Serial.println( take_value_optic_A.get_steps() );
+    }
   }
-  if( take_value_optic_B.have_taked_steps() ){
-    Serial.print("steps B taken:");
-    Serial.println( take_value_optic_B.get_steps() );
+
+  if( c == 'b' ){
+    if( !optic_B_attiny85.isListening() )
+      optic_B_attiny85.listen();
+
+    optic_B_attiny85.write( "#p\n" );
+
+    take_value_optic_B.take();
+
+    if( take_value_optic_B.have_taked_speed() ){
+      Serial.print("speed B taken:");
+      Serial.println( take_value_optic_B.get_speed() );
+    }
+    if( take_value_optic_B.have_taked_steps() ){
+      Serial.print("steps B taken:");
+      Serial.println( take_value_optic_B.get_steps() );
+    }
   }
-*/
 }
